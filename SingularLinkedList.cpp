f2b9cfef88f4c0a32ae1ec7c2f3f45381e61ac4e
Daniel Savage <ds759@bath.ac.uk>
2019-08-02 12:57:28 +0100

    Added get first functionality

--- SingularLinkedList.cpp
+++ SingularLinkedList.cpp
@@ -19,43 +19,83 @@ public:
     Node *head;
     Node *tail;
 
-    LinkedList()
-    {
-        this->head = nullptr;
-        this->tail = nullptr;
-    }
+    LinkedList();
 
-    void add_node(int data)
-    {
-        Node *node = new Node(data);
-        if (this->head == nullptr)
-        {
-            this->head = node;
-        }
-        else
-        {
-            this->tail->next = node;
-        }
-        this->tail = node;
-    }
+    void add_first(int data);
+    void add(int data);
+    void add(int data, int index);
 
-    void print()
-    {
-        while (this->head)
-        {
-            std::cout << this->head->data << '\n';
-            this->head = this->head->next;
-        }
-    }
+    int get_first();
+    void print();
 };
+
+LinkedList::LinkedList(void)
+{
+    this->head = nullptr;
+    this->tail = nullptr;
+}
+
+void LinkedList::add_first(int data)
+{
+    Node *node = new Node(data);
+    node->next = this->head;
+    this->head = node;
+}
+void LinkedList::add(int data)
+{
+    Node *node = new Node(data);
+    if (this->head == nullptr)
+    {
+        this->head = node;
+    }
+    else
+    {
+        this->tail->next = node;
+    }
+    this->tail = node;
+}
+
+void LinkedList::add(int data, int index)
+{
+    Node *node = new Node(data);
+    int i = 1;
+    Node *n = this->head;
+    while (i < index)
+    {
+        n = n->next;
+        i++;
+    }
+    node->next = n->next;
+    n->next = node;
+}
+
+int LinkedList::get_first()
+{
+    return this->head->data;
+}
+
+void LinkedList::print()
+{
+    Node *temp = this->head;
+    while (this->head)
+    {
+        std::cout << this->head->data << '\n';
+        this->head = this->head->next;
+    }
+    this->head = temp;
+}
 
 int main()
 {
     LinkedList *llist = new LinkedList();
-    llist->add_node(1);
-    llist->add_node(2);
-    llist->add_node(3);
-    llist->add_node(4);
-    llist->add_node(5);
+    llist->add(1);
+    llist->add(2);
+    llist->add(4);
+    llist->add(5);
+    llist->add(6);
+    llist->add(3, 2);
+    llist->add_first(0);
     llist->print();
+    int first = llist->get_first();
+    std::cout << first << '\n';
 }
